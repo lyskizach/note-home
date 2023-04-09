69a654f620865bfa9cd036c51cbe6b60da3388fe
lyskz <lyski.zach@icloud.com>
2023-04-09 12:14:10 -0700

    add starting variables and stuff

---
+++ Develop/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
