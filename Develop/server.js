d63a810c85b2ef32ce02b2f7de8ad7589a507c64
lyskz <lyski.zach@icloud.com>
2023-04-09 12:25:49 -0700

    added get requests

--- Develop/server.js
+++ Develop/server.js
@@ -6,3 +6,15 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+);
